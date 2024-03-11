47dcb421cff70a5a75a1ad6afe3b3f0b067f1a3d
Sanjeedh20 <112870456+Sanjeedh20@users.noreply.github.com>
2024-03-11 14:16:56 +0530

    Update BusConnect.java

--- Jdbc/src/BusRev/BusConnect.java
+++ Jdbc/src/BusRev/BusConnect.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class BusConnect {
 	private static final String url = "jdbc:mysql://localhost:3306/demo";
 	private static final String userName = "root";
-	private static final String passWord = "pubg@123";
+	private static final String passWord = "12345";
 	
 	public static Connection connect() throws SQLException {
 		return DriverManager.getConnection(url, userName, passWord);
